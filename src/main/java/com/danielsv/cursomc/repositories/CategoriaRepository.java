2ee262b4e7425bbfed635c8de548cd7788cd138d
danielsv <daniel_vs@msn.com>
2017-12-16 23:36:38 +0000

    Criando repository e service para Categoria

---
+++ src/main/java/com/danielsv/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.danielsv.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.danielsv.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
