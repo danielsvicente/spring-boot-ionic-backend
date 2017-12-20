32bbc8741edcb80468dae46bcb257ed3c93bf6d6
danielsv <daniel_vs@msn.com>
2017-12-20 23:55:18 +0000

    Deleção de categoria e exception personalizada

--- src/main/java/com/danielsv/cursomc/services/CategoriaService.java
+++ src/main/java/com/danielsv/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.danielsv.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.danielsv.cursomc.domain.Categoria;
 import com.danielsv.cursomc.repositories.CategoriaRepository;
+import com.danielsv.cursomc.services.exceptions.DataIntegrityException;
 import com.danielsv.cursomc.services.exceptions.ObjectNotFoundExpection;
 
 @Service
@@ -30,5 +32,15 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos associados.");
+		}
+		
 	}
 }
